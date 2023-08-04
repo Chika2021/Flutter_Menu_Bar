e805ebb89b5ecd061957a6105835ee5514cb0c3a
Chika2021 <amaechichika9@gmail.com>
2023-08-04 02:49:10 +0100

    Creating List of Items

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,15 @@ class Dynamic extends StatefulWidget {
 }
 
 class _DynamicState extends State<Dynamic> {
+  final List<String> _items = ['item 1', 'item 2', 'itme 3', 'item 4', 'item 5'];
+  final List<String> _cartItems = [];
+
+  void addItemToCart(String item) {
+    setState(() {
+      _cartItems.add(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,42 +42,55 @@ class _DynamicState extends State<Dynamic> {
           //   ])
           // ],
 
-
-          actions:   [
-            DropdownButton( items:const  [
-               DropdownMenuItem(value:'Option 1' , child: Text('Option 1') ),
-               DropdownMenuItem(value: 'Option 2', child: Text('Option 2')),
-               DropdownMenuItem(value:'Option 3', child: Text('Option 3'))
-
-            ], onChanged: (newValue) => {
-
-            }, hint: const Text('Visit US' , style:TextStyle(color: Colors.white),) ),
-
-              PopupMenuButton<String>(itemBuilder: (context) => [
-              const PopupMenuItem(value: 'First', child: Text('First')),
-              const PopupMenuItem(value:'Seconst', child: Text('Second')),
-              const PopupMenuItem(value:'Third', child: Text('Third'))
-            ]),
-              IconButton(
-                icon: const Icon(Icons.search),
-                onPressed: () {
-                  // Handle search action
-                  print('Search action pressed');
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.settings),
-                onPressed: () {
-                  // Handle settings action
-                  print('Settings action pressed');
-                },
-              ),
-            ],
-
-
+          actions: [
+            DropdownButton(
+                items: const [
+                  DropdownMenuItem(value: 'Option 1', child: Text('Option 1')),
+                  DropdownMenuItem(value: 'Option 2', child: Text('Option 2')),
+                  DropdownMenuItem(value: 'Option 3', child: Text('Option 3'))
+                ],
+                onChanged: (newValue) => {},
+                hint: const Text(
+                  'Visit US',
+                  style: TextStyle(color: Colors.white),
+                )),
+            PopupMenuButton<String>(
+                itemBuilder: (context) => [
+                      const PopupMenuItem(value: 'First', child: Text('First')),
+                      const PopupMenuItem(
+                          value: 'Seconst', child: Text('Second')),
+                      const PopupMenuItem(value: 'Third', child: Text('Third'))
+                    ]),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                // Handle search action
+                print('Search action pressed');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                // Handle settings action
+                print('Settings action pressed');
+              },
+            ),
+          ],
         ),
-        body: Center(
-            child: CupertinoButton(
-                onPressed: () {}, child: const Text('Cupertino Button'))));
+        body: ListView.builder(
+            itemCount: _items.length,
+            itemBuilder: (context, index) {
+              final item = _items[index];
+              final isInCart = _cartItems.contains(item);
+
+              return ListTile(
+                title: Text(item),
+                trailing: isInCart  ? Icon(Icons.check_circle , color:Colors.green)
+                                    : IconButton(icon:Icon(Icons.add_circle)  , 
+                                    onPressed:() => addItemToCart(item)),
+              );
+
+
+            }));
   }
 }
